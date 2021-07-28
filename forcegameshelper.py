a5734ecf38ce831cad0ce55b4bf2e6e37077ef37
PixelDG <35879142+pixeldashgames@users.noreply.github.com>
2021-07-28 16:48:46 -0400

    Update forcegameshelper.py

--- forcegameshelper.py
+++ forcegameshelper.py
@@ -3,43 +3,22 @@ import requests
 import os
 
 def start(bot, update):
-    update.message.reply_text('Ahoy {}! Welcome to JBossStarsBot. \n\nTo get started, use the /stars command to fetch the stars from the GitHub repos of JBoss'.format(update.message.from_user.first_name))
+    update.message.reply_text('Hola {}! Añademe a un canal para utilizarme!'.format(update.message.from_user.first_name))
 
+def receive_message(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, out='received')	
+	
+	
 
-def stars(bot, update):
-    api = requests.get('https://api.github.com/orgs/JBossOutreach/repos')
-    json = api.json()
-    stars = ''
-    for i in range(len(json)):
-        stars = stars + '\n' + json[i]['name'] + ' : ' + str(json[i]['stargazers_count'])
-
-    update.message.reply_text('Here\'s the list of all the JBoss repositories on GitHub along with their respective star count. \n\n' + stars + '\n\nTo get the stars of a specific repository, enter the name of the repository.')
-
-
-def repo_stars(bot, update):
-    api = requests.get('https://api.github.com/orgs/JBossOutreach/repos')
-    json = api.json()
-    star = ''
-    for i in range(len(json)):
-        cur = json[i]['name']
-        if cur == update.message.text:
-            star += cur + ' : ' + str(json[i]['stargazers_count'])
-            break
-        elif cur == '':
-            star = 'No such repository found.'
-
-    bot.send_message(chat_id=update.message.chat_id, out=star)
-
-TOKEN = ''
+TOKEN = '1609173394:AAH4V-ShZIGXOBE9JPOUg3wVN_Q3BycCZG4'
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)                           
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('stars', stars))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, repo_stars))
+updater.dispatcher.add_handler(MessageHandler(Filters.chat_type.channel, receive_message))
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-updater.bot.set_webhook("https://gcijbossbot.herokuapp.com/" + TOKEN)
+updater.bot.set_webhook("https://forcegameshelper.herokuapp.com/" + TOKEN)
 updater.idle()
