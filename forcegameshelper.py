9981daf22defc2c0cbc7bb4d082af75805c4137e
PixelDG <35879142+pixeldashgames@users.noreply.github.com>
2021-07-28 22:11:00 -0400

    Update forcegameshelper.py

--- forcegameshelper.py
+++ forcegameshelper.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ExtBot
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
@@ -10,19 +10,30 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '1609173394:AAH4V-ShZIGXOBE9JPOUg3wVN_Q3BycCZG4'
 
+admin_chat_id = -1
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
+    if len(context.args) >= 2 and context.args[0] == "admin" and context.args[1] == TOKEN:
+        update.message.reply_text("Logged in as Admin!")
+        global admin_chat_id
+        admin_chat_id = update.message.chat.id
+
 
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def process_message(update, context):
+    """Send message to admin."""
+    if admin_chat_id != -1:
+        update.message.reply_text("Enviando a admin")
+        context.bot.send_message(chat_id=admin_chat_id, text=update.message.text)
+    else:
+        update.message.reply_text(update.message.text)
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -43,7 +54,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, process_message))
 
     # log all errors
     dp.add_error_handler(error)
