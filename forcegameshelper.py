a56784a95f4ec4641da3bb3e2718e9fd1f6795d5
PixelDG <35879142+pixeldashgames@users.noreply.github.com>
2021-07-28 22:15:16 -0400

    Update forcegameshelper.py

--- forcegameshelper.py
+++ forcegameshelper.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ExtBot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
